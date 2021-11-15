cea026d67cd5ab8e757bfdbffc0ad3211674ed2c
Patrick-Ko <komladogbatse28@gmail.com>
2021-11-15 16:56:56 +0000

    styles

---
+++ components/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+
+//colors
+export const Colors = {
+    primary: "#ffffff",
+    secondary: "#E5E7EB",
+    tertiary: "#1F2937",
+    darklight: "#9CA3AF",
+    brand: "#6D28D9",
+    green: "#10B981",
+    red: "#EF4444",
+};
+
+const {primary, secondary, tertiary, darklight, brand, green, red } = Colors;
+
